01333e5212553842ab147a69c6c88d6367adfd82
DESKTOP-KVCQHCD\multicampus <dkwjdiq@naver.com>
2021-04-13 00:52:46 +0900

    JavaScript 4장 프로토타입 체이닝 마무리

---
+++ JavaScript/함수와 프로토타입 체이닝/함수호출과 this/프로토타입체이닝/프로토타입 메서드 this바인딩/function.js
@@ -0,0 +1,18 @@
+//Person() 생성자 함수
+function Person(name) {
+    this.name = name;
+}
+
+//getName() 프로토타입 메서드
+Person.prototype.getName = function () {
+    return this.name;
+}
+
+//foo 객체 생성
+var foo = new Person('foo');
+console.log(foo.getName()); //foo
+
+//Person.prototype 객체에 name 프로퍼티 동적 추가
+console.dir(Person.prototype);
+Person.prototype.name = 'person';
+console.log(Person.prototype.getName()); //person
